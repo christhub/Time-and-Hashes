03527d3a84427b65c6160c0ebba85065442fcb64
christian lei & carlo de gregorio <student@epicodus.com>
2015-08-04 10:48:34 -0700

    one passing test, writing 2nd failing test

---
+++ lib/sleep_time.rb
@@ -0,0 +1,5 @@
+class Time
+  define_method(:sleep_time) do
+
+  end
+end
---
+++ spec/sleep_time_spec.rb
@@ -0,0 +1,14 @@
+require('rspec')
+require('sleep_time')
+
+describe('Time#sleep_time') do
+  # it("displays the time and date") do
+  #   test_date = Time.new(2015)
+  #   expect(test_date.sleep_time()).to(eq("2015-01-01 00:00:00 -0800"))
+  # end
+
+  it('tests whether or not it is a weekend day') do
+    test_date = Time.new(2015)
+    expect(test_date.sleep_time()).to(eq("sleep in"))
+  end
+end
